48250f3587c33f36339adf87d85fe852c396b8d5
Johan <johan131105@gmail.com>
2026-05-27 14:18:04 -0600

    Creacion del proyecto

---
+++ src/main/java/BandSync/BandSyncApplication.java
@@ -0,0 +1,13 @@
+package BandSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BandSync/BandSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package BandSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
