0b49bb89660e84a099f7464fac50bee318cb69ba
Johan <johan131105@gmail.com>
2026-06-06 14:43:54 -0600

    Traer Ensayos desde Andres

--- src/main/java/BandSync/Model/Ensayos/Ensayos.java
+++ src/main/java/BandSync/Model/Ensayos/Ensayos.java
@@ -1,4 +1,49 @@
 package BandSync.Model.Ensayos;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table (name = "tb_Ensayos")
 public class Ensayos {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+
+    private Integer id;
+    private LocalDate date;
+    private String section;
+
+    public Ensayos() {
+    }
+
+    public Ensayos(Integer id, LocalDate date, String section) {
+        this.id = id;
+        this.date = date;
+        this.section = section;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
 }
