1d98a68addf1dc338c22ddb608b7aa406fbefa1a
Johan <johan131105@gmail.com>
2026-06-06 15:18:29 -0600

    Model Presentaciones con sus relaciones

--- src/main/java/BandSync/Model/Presentaciones.java
+++ src/main/java/BandSync/Model/Presentaciones.java
@@ -1,5 +1,82 @@
 package BandSync.Model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
 public class Presentaciones {
+
+    @Entity
+    @Table(name = "tb_presentaciones")
+    public class Presentaciones {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id", nullable = false)
+        private Integer id;
+
+        @Column(name = "fecha", nullable = false)
+        private LocalDate date;
+
+        @Column(name = "ubicacion", nullable = false, length = 100)
+        private String location;
+
+        @ManyToOne
+        @JoinColumn(name = "id_integrante", nullable = false, foreignKey = @ForeignKey(name = "fk_presentacion_integrante"))
+        private Integrantes integrante;
+
+        @Column(name = "asistencia", nullable = false, length = 20)
+        private String asistencia;
+
+        public Presentaciones() {
+        }
+
+        public Presentaciones(LocalDate date, String location, Integrantes integrante, String asistencia) {
+            this.date = date;
+            this.location = location;
+            this.integrante = integrante;
+            this.asistencia = asistencia;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public LocalDate getDate() {
+            return date;
+        }
+
+        public void setDate(LocalDate date) {
+            this.date = date;
+        }
+
+        public String getLocation() {
+            return location;
+        }
+
+        public void setLocation(String location) {
+            this.location = location;
+        }
+
+        public Integrantes getIntegrante() {
+            return integrante;
+        }
+
+        public void setIntegrante(Integrantes integrante) {
+            this.integrante = integrante;
+        }
+
+        public String getAsistencia() {
+            return asistencia;
+        }
+
+        public void setAsistencia(String asistencia) {
+            this.asistencia = asistencia;
+        }
+    }
 
 }
