6866ce266df95279f5ffd4b622dcc94a4b9fb6fa
Johan <johan131105@gmail.com>
2026-06-07 13:31:24 -0600

    Arreglo de model Instrumentos (faltaba set name) e implemento el InstrumentosRepository

--- src/main/java/BandSync/Repository/Instrumentos/InstrumentosRepository.java
+++ src/main/java/BandSync/Repository/Instrumentos/InstrumentosRepository.java
@@ -3,5 +3,10 @@ package BandSync.Repository.Instrumentos;
 import BandSync.Model.Instrumentos.Instrumentos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InstrumentosRepository extends JpaRepository <Instrumentos, Integer>{
+    List <Instrumentos> findByName (String name);
+    List <Instrumentos> findByCondition (String condition);
+    List <Instrumentos> findByAvailability (String availability);
 }
