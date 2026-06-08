32449a3f3f52d34135b60d579770a0504066b630
Johan <johan131105@gmail.com>
2026-06-07 19:44:35 -0600

    corrección error ortográfico

--- src/main/java/BandSync/Repository/Presentaciones/PresentacionesRepository.java
+++ src/main/java/BandSync/Repository/Presentaciones/PresentacionesRepository.java
@@ -1,6 +1,5 @@
 package BandSync.Repository.Presentaciones;
 
-import BandSync.Model.Ensayos.Ensayos;
 import BandSync.Model.Presentaciones.Presentaciones;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +8,5 @@ import java.util.List;
 
 public interface PresentacionesRepository extends JpaRepository <Presentaciones, Integer> {
     List<Presentaciones> findByDate (LocalDate date);
-    List<Presentaciones> findByLoction (String location);
+    List<Presentaciones> findByLocation(String location);
 }//fin
