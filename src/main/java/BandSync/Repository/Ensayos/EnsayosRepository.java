00ea9d859f5ff7f8882bb3e3da8dcd213c5864b7
Johan <johan131105@gmail.com>
2026-06-10 15:37:11 -0600

    las anotaciones de los repositorios no se porque no se agregaron al commit anterior

--- src/main/java/BandSync/Repository/Ensayos/EnsayosRepository.java
+++ src/main/java/BandSync/Repository/Ensayos/EnsayosRepository.java
@@ -2,10 +2,11 @@ package BandSync.Repository.Ensayos;
 
 import BandSync.Model.Ensayos.Ensayos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
-
+@Repository
 public interface EnsayosRepository extends JpaRepository <Ensayos, Integer> {
     List<Ensayos> findByDate(LocalDate date);
     List<Ensayos> findBySection(String section);
--- src/main/java/BandSync/Repository/Presentaciones/PresentacionesRepository.java
+++ src/main/java/BandSync/Repository/Presentaciones/PresentacionesRepository.java
@@ -3,10 +3,11 @@ package BandSync.Repository.Presentaciones;
 import BandSync.Model.Ensayos.Ensayos;
 import BandSync.Model.Presentaciones.Presentaciones;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
-
+@Repository
 public interface PresentacionesRepository extends JpaRepository <Presentaciones, Integer> {
     List<Presentaciones> findByDate (LocalDate date);
     List<Presentaciones> findByLocation(String location);
